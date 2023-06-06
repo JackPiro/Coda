3ce587f6d980f348d022785e14ccb25ac89c2f06
Jack Piro <jackpiro1133@gmail.com>
2023-06-06 11:20:31 -0700

    albums model seperated from music music model === song model

--- server/models/Album.js
+++ server/models/Album.js
@@ -1,21 +1,18 @@
-const mongoose =  require('mongoose');
+const mongoose = require('mongoose');
 
-// Now we Define the Music Schema music === song should prolly rename this later bc we have songs ad ablums
-
-// mongoose.Schema is a method that takes an object
-const MusicSchema = new mongoose.Schema({
+const AlbumSchema = new mongoose.Schema({
     artistID: {type: mongoose.Schema.Types.ObjectId, ref: 'Artist'},
+    songs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Music' }],
     title: {type: String, required: true},
     description: {type: String},
     genre: {type: String, required: true},
     coverArt: {type: String, required: true},
-    audioFile: {type: String, required: true},
     offer: {type: Boolean, required: true, default: false},
     releaseDate: {type: Date, required: true, default: Date.now},
-    createdAt: {type: Date, default: Date.now},
     NFTType: {type: String, enum: ['royaltySharing', 'collectible', 'none'], default: 'none'},
+    createdAt: {type: Date, default: Date.now},
     updatedAt: {type: Date, default: Date.now}
-});
+})
 
-const Music = mongoose.model('Music', MusicSchema);
-module.exports = Music;
+const Album = mongoose.model('Album', AlbumSchema);
+module.exports = Album
