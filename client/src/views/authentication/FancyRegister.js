335bac322638d8426130cd260e14a1ae61a4f74d
Jack Piro <jackpiro1133@gmail.com>
2023-06-28 21:17:59 -0700

    trying to get new register to work

--- client/src/views/authentication/FancyRegister.js
+++ client/src/views/authentication/FancyRegister.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import { React, useState } from 'react';
 import '../../components/shared/button/newButton.css'
 import PersonIcon from '../../assets/Icons/PersonIcon';
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+import authService from '../../services/authService';
 
 
 const FancyRegister = () => {
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [role, setRole] = useState('listener');
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true);
+        setErrorMessage('');
+    
+        try {
+            await authService.register(firstName, lastName, email, username, password, role);
+            navigate('/home');
+        } catch (error) {
+            if (error.response && error.response.data && error.response.data.message) {
+                setErrorMessage(error.response.data.message);
+            } else {
+                setErrorMessage('An error occurred while registering. Please try again later.');
+            }
+        } finally {
+            setLoading(false);
+        }
+    };
+
+
     return (
         <div>
             <div class="min-w-screen min-h-screen bg-gray-900 flex items-center justify-center px-5 py-5">
@@ -47,16 +81,43 @@ const FancyRegister = () => {
                                 </div>
                                 <div class="flex -mx-3">
                                     <div class="w-full px-3 mb-12">
-                                        <label for="" class="text-xs font-semibold px-1">Password</label>
+                                        <label for="" class="text-xs font-semibold px-1">Create Password</label>
                                         <div class="flex">
                                             <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
                                             <input type="password" class="w-full -ml-10 pl-10 pr-3 py-2 bg-[#181C25] rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="************" />
                                         </div>
                                     </div>
                                 </div>
+                                <div class="flex -mx-3 -mt-6">
+                                    <div class="w-full px-3 mb-12">
+                                        <label for="" class="text-xs font-semibold px-1">Create Unique Username</label>
+                                        <div class="flex">
+                                            <div class="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"><i class="mdi mdi-lock-outline text-gray-400 text-lg"></i></div>
+                                            <input type="password" class="w-full -ml-10 pl-10 pr-3 py-2 bg-[#181C25] rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500" placeholder="MakeItGood" />
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                <div className=" flex mb-10 space-x-8">
+                                    <button
+                                        onClick={() => setRole("listener")}
+                                        className={`py-1 px-4 w-1/3 text-sm rounded-lg shadow-md text-white ${role === "listener" ? "bg-[#336CFF]" : "bg-[#202530]"}`}
+                                    >
+                                        Enroll as Listener
+                                    </button>
+                                    <div className='w-1/3'>or</div>
+                                    <button
+                                        onClick={() => setRole("artist")}
+                                        className={`py-1 px-4 w-1/3 text-sm rounded-lg shadow-md text-white ${role === "artist" ? "bg-[#336CFF]" : "bg-[#202530]"}`}
+                                    >
+                                        Enroll as Artist
+                                    </button>
+                                </div>
+
+
                                 <div class="flex -mx-3">
                                     <div class="w-full px-3 mb-5">
-                                        <button class="block w-full px-2 py-2 max-w-xs mx-auto register-button text-white rounded-lg font-semibold text-sm">See you Inside</button>
+                                        <button type='submit' onClick={handleSubmit} class="block w-full px-2 py-2 max-w-xs mx-auto register-button text-white rounded-lg font-semibold text-sm">See You Inside</button>
                                     </div>
                                 </div>
                             </div>
--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
-    username: { type: String, required: true, unique: true },
+    //removed required for the time being
+    username: { type: String, require: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['artist', 'listener'], required: true },
