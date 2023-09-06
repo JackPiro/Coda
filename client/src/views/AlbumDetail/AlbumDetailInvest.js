165bf736a7fe88745d7d2d153dda02056764029a
Jack Piro <jackpiro1133@gmail.com>
2023-09-06 13:36:44 -0700

    update

--- client/src/views/AlbumDetail/AlbumDetailInvest.js
+++ client/src/views/AlbumDetail/AlbumDetailInvest.js
@@ -61,6 +61,8 @@ const AlbumDetailInvest = () => {
                                     <LineSongDisplay />
                                     <LineSongDisplay />
 
+
+
                                     
                                 </div>
                             </div>
