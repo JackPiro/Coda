9cc4d34d2d06e962d239f2f9cc9acf923e15b6e0
Jack Piro <jackpiro1133@gmail.com>
2023-09-19 15:45:20 -0700

    v2 stripe with help modded

--- server/utils/subscriptionHandler.js
+++ server/utils/subscriptionHandler.js
@@ -3,6 +3,9 @@ const User = require('../models/User');
 
 module.exports.subscribe = async (req, res) => {
     try {
+
+        const userId = req.user._id;
+
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ['card'],
             line_items: [{
@@ -12,6 +15,9 @@ module.exports.subscribe = async (req, res) => {
             mode: 'subscription',
             success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}', // Replace with your actual website URL
             cancel_url: 'https://example.com/cancel', // Replace with your actual website URL
+            metadata: {
+                userId: userId.toString()
+            }
         });
 
         res.json({ sessionId: session.id });
@@ -26,12 +32,11 @@ module.exports.successfullySubscribed = async (req, res) => {
         const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
         
         if (session.mode === 'subscription' && session.payment_status === 'paid') {
-            // TODO: Extract the user's ID from the session or from your authentication middleware 
-            const userId = // Fetch user ID logic goes here...
+            const userId = req.user._id
 
             // Retrieve the user from the database, update their subscription status, and store Stripe's subscription ID
             const user = await User.findById(userId);
-            user.platformSubscriptionStatus = 'active';
+            user.platformSubscription.status = 'active';
             user.platformSubscription.stripeSubscriptionId = session.subscription;
             await user.save();
 
@@ -48,8 +53,7 @@ module.exports.successfullySubscribed = async (req, res) => {
 
 module.exports.successfullyUnsubscribed = async (req, res) => {
     try {
-        // TODO: Extract the user's ID from your authentication middleware 
-        const userId = // Fetch user ID logic goes here...
+        const userId = req.user._id;
 
         // Retrieve the specific user from the database and get their associated Stripe subscription ID
         const user = await User.findById(userId);
@@ -59,10 +63,10 @@ module.exports.successfullyUnsubscribed = async (req, res) => {
             await stripe.subscriptions.del(stripeSubscriptionId);
 
             // Update the user's subscription status in the database
-            user.platformSubscriptionStatus = 'canceled';
+            user.platformSubscription.status = 'canceled';
             await user.save();
 
-            res.json({ status: 'unsubscribed' });
+            res.json({ status: 'canceled' });
         } catch (err) {
             console.error("Error cancelling Stripe subscription:", err);
             res.status(500).json({ error: 'Failed to cancel subscription' });
