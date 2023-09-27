7db4c5529edcc5cdadeff758c01fd561d5cb3427
Jack Piro <jackpiro1133@gmail.com>
2023-09-27 11:59:50 -0700

    working on loyalty points system

--- server/controllers/NFTPointsBundle.controller.js
+++ server/controllers/NFTPointsBundle.controller.js
@@ -1,20 +1,40 @@
 const NFTPointsBundle = require('../models/NFTPointsBundle');
 const User = require('../models/User');
-//... Other required models
+const Artist = require('../models/Artist')
 
 // 1. Create NFT Bundle
 module.exports.createNFTBundle = async (req, res) => {
     try {
         const { userId, artistId, points } = req.body;
+
+        // Validate the user and artist
+        const user = await User.findById(userId);
+        const artist = await Artist.findById(artistId);
+        if (!user || !artist) {
+            return res.status(404).json({ message: 'User or artist not found' });
+        }
+
+        // Check if the user has enough loyalty points
+        if (user.loyaltyPoints < points) {
+            return res.status(400).json({ message: 'Not enough loyalty points to create the NFT bundle' });
+        }
+
+        // Optional: Rate limiting or minimum threshold for NFT creation can be added here.
+
+        // Deduct loyalty points from user's total and update in the database
+        user.loyaltyPoints -= points;
+        await user.save();
+
+        // Create the NFT bundle
         const nftBundle = new NFTPointsBundle({ userId, artistId, points });
         await nftBundle.save();
+
         return res.json(nftBundle);
     } catch(err) {
         console.error(err);
         return res.status(500).json({ message: 'Server error' });
     }
 };
-
 // 2. List NFT Points Bundle for Sale
 module.exports.listNFTForSale = async (req, res) => {
     try {
