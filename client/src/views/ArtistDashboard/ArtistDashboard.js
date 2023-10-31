57a0ed468584abd603936465074b842f2939c184
Jack Piro <jackpiro1133@gmail.com>
2023-10-31 12:29:19 -0700

    update

--- client/src/views/ArtistDashboard/ArtistDashboard.js
+++ client/src/views/ArtistDashboard/ArtistDashboard.js
@@ -38,6 +38,8 @@ function ArtistDashboard() {
     const query = useQuery();
     const code = query.get('code');
 
+    
+
     useEffect(() => {
         if (code) {
             // If there's a code in the URL, send it to the backend
